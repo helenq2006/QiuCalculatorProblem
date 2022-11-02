727ed3e61a10036bdafcd8e0b27a9add0ee73d60
Helen Qiu <helenq2@nycstudents.net>
2022-11-01 21:03:32 -0400

    final commit!

--- src/Runner.java
+++ src/Runner.java
@@ -7,7 +7,7 @@ public class Runner {
         String expression = scan.nextLine();
 
 
-        String first = expression.substring(0, expression.indexOf(" ") + 1);
+        String first = expression.substring(0, expression.indexOf(" "));
         int firstNum = Integer.parseInt(first);
 
         String operator = expression.substring(expression.indexOf(" ") + 1, expression.indexOf(" ") + 2);
@@ -15,10 +15,22 @@ public class Runner {
         String second = expression.substring(expression.indexOf(" ") + 3);
         int secondNum = Integer.parseInt(second);
 
+
+        Calculator calc = new Calculator(firstNum,secondNum);
+
         if (operator.equals("+")) {
-
+            System.out.println(calc.add());
+        } else if (operator.equals("-")) {
+            System.out.println(calc.subtract());
+        } else if (operator.equals("*")) {
+            System.out.println(calc.multiply());
+        } else if (operator.equals("/")) {
+            System.out.println(calc.divide());
+        } else if (operator.equals("^")) {
+            System.out.println(calc.exponent());
+        } else {
+            System.out.println(calc.mod());
         }
-
 
     }
 }
